ed6f0fabfaa955cfc4a4b593fbdcaa32574abfa0
Yakushev Roman <boxboxbox@inbox.ru>
2022-08-31 22:57:08 +0300

    Fix Data to 7 days week

--- script.js
+++ script.js
@@ -5,17 +5,31 @@ async function getWeatherByCityName(){
     let cityName = $('#city').val();
     let response = await fetch(`${apiUrl}?q=${cityName}&appid=${apiKey}&units=metric`);
     let data = await response.json();
-    let dataCount = data.list;
+    //let dataCount = data.list;
     let weekDate = [];
 
-    //console.log(dataCount);
+    console.log(data);
 
     for (let i = 1; i < 7; i++) { 
         let followingDay = new Date(Date.now() + (86400000 *i));
         weekDate.push(followingDay.toISOString().slice(0,10));
     }
 
-    dataCount.forEach(item => {
+    //console.log(weekDate);
+
+    let itemsArr = [];
+    let count = 1;
+
+    
+    for (const item of data.list) {
+        if (weekDate.includes(item.dt_txt.slice(0, 10))) {
+            count++;
+            itemsArr.push(item);
+            weekDate = weekDate.filter(data=>data!=item.dt_txt.slice(0,10));
+        }
+    }
+
+    for (const item of itemsArr){
         $('.cards').append($('<div>',{'class': 'card'}))
         .append($(`<img src="http://openweathermap.org/img/wn/${item.weather[0].icon}.png" class="card-img-top" alt="...">`))
         .append($('<div>',{'class': 'card-body'}).append($(`<h5 class="card-title">${data.city.name}  :  ${Math.round(item.main.temp)} ℃</h5>`))
@@ -23,7 +37,7 @@ async function getWeatherByCityName(){
         .append($(`<p class="card-text">Max temperature : ${Math.round(item.main.temp_max)} ℃</p> `))
         .append($(`<p class="card-text">Pressure : ${Math.round(item.main.pressure)}</p> `))
         .append($(`<p class="card-text">Humidity : ${Math.round(item.main.humidity)}</p>`)));
-    });
+    }
 }
 
 addEventListener('submit',function (){
