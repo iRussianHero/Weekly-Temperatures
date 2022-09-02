5fcc1f2fc4901b560efc0c8dcf57b79ae95249aa
Yakushev Roman <boxboxbox@inbox.ru>
2022-09-02 22:08:42 +0300

    ReDesign

--- script.js
+++ script.js
@@ -30,13 +30,12 @@ async function getWeatherByCityName(){
     }
 
     for (const item of itemsArr){
-        $('.cards').append($('<div>',{'class': 'card'}))
-        .append($(`<img src="http://openweathermap.org/img/wn/${item.weather[0].icon}.png" class="card-img-top" alt="...">`))
+        $('.cards').append(($('<div>',{'class': 'card'})).append($(`<img src="http://openweathermap.org/img/wn/${item.weather[0].icon}.png" class="card-img-top" alt="...">`))
         .append($('<div>',{'class': 'card-body'}).append($(`<h5 class="card-title">${data.city.name}  :  ${Math.round(item.main.temp)} ℃</h5>`))
         .append($(`<p class="card-text">Min temperature : ${Math.round(item.main.temp_min)} ℃</p>`))
         .append($(`<p class="card-text">Max temperature : ${Math.round(item.main.temp_max)} ℃</p> `))
         .append($(`<p class="card-text">Pressure : ${Math.round(item.main.pressure)}</p> `))
-        .append($(`<p class="card-text">Humidity : ${Math.round(item.main.humidity)}</p>`)));
+        .append($(`<p class="card-text">Humidity : ${Math.round(item.main.humidity)}</p>`))));
     }
 }
 
